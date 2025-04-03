c604a0af3b81b5980471ff4fb19ec3f740ebcb0e
KYWILS21 <tuk30460@temple.edu>
2025-04-03 12:16:47 -0400

    Implemented timer function in main activity.

--- app/src/main/java/edu/temple/myapplication/MainActivity.kt
+++ app/src/main/java/edu/temple/myapplication/MainActivity.kt
@@ -1,21 +1,56 @@
 package edu.temple.myapplication
 
+import android.content.ComponentName
+import android.content.Intent
+import android.content.ServiceConnection
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.IBinder
+import android.os.Looper
 import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    var timerBinder: TimerService.TimerBinder? = null
+
+    val handler = Handler(Looper.getMainLooper()) { msg ->
+        findViewById<TextView>(R.id.textView).text = msg.what.toString()
+        true
+    }
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        findViewById<Button>(R.id.startButton).setOnClickListener {
+        bindService(
+            Intent(this, TimerService::class.java),
+            object: ServiceConnection{
+                override fun onServiceConnected(name: ComponentName?, service: IBinder?) {
+                    (service as TimerService.TimerBinder).setHandler(handler)
+                }
 
+                override fun onServiceDisconnected(name: ComponentName?) {
+                    timerBinder = null
+                }
+            },
+            BIND_AUTO_CREATE
+        )
+
+        findViewById<Button>(R.id.startButton).setOnClickListener {
+            timerBinder?.run {
+                if(!isRunning){
+                    start(10)
+                } else {
+                    pause()
+                }
+            }
         }
         
         findViewById<Button>(R.id.stopButton).setOnClickListener {
-
+            timerBinder?.stop()
         }
     }
 }
